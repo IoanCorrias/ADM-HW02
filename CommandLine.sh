f1871e13183dee4c68045605b818f483db0b65a8
Ioan Corrias <ioan27corrias@gmail.com>
2022-11-20 23:39:34 +0100

    Add files via upload

--- CommandLine.sh
+++ CommandLine.sh
@@ -4,8 +4,16 @@ pip install csvkit
 
 clear
 
-csvcut --tabs -c post_id,profile_id,description instagram_posts.csv | csvgrep -c description -r '.{100,}' | head --lines 11 > selected_posts.csv
+for country in "Italy" "Spain" "France" "England" "United States"
+do
+	echo "Number of places found in $country:"
+	csvcut --tabs -c placeAddress,numPeopleVisited,numPeopleWant places.tsv | csvgrep -c placeAddress -r "$country" | csvstat -c 1 --unique
+	
+	echo "Average number of visits in $country:"
+	csvcut --tabs -c placeAddress,numPeopleVisited,numPeopleWant places.tsv | csvgrep -c placeAddress -r "$country" | csvstat -c 2 --mean
+	
+	echo "Sum of people who want to visit $country:"
+	csvcut --tabs -c placeAddress,numPeopleVisited,numPeopleWant places.tsv | csvgrep -c placeAddress -r "$country" | csvstat -c 3 --sum
 
-csvjoin -c profile_id selected_posts.csv instagram_profiles.csv > merged.csv
-
-csvcut -c profile_id,profile_name merged.csv | csvlook
+	echo 
+done
